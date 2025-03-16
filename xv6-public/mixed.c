9fe3e945aa9258f21086ef6feed46260e8dee6fe
Aayush Beura <90240872+AayushBeura@users.noreply.github.com>
2025-03-16 12:08:36 +0530

    Add files via upload

---
+++ xv6-public/mixed.c
@@ -0,0 +1,16 @@
+// mixed.c
+#include "types.h"
+#include "user.h"
+
+int main() {
+  printf(1, "Mixed workload process %d starting\n", getpid());
+  for(int i = 0; i < 10; i++) {
+    int x = 0;
+    // Compute for a while
+    for(int j = 0; j < 100000000; j++) {
+      x += j;
+    }
+    sleep(1000);
+  }
+  exit();
+}
